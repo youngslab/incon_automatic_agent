6acea68e1c97af9dbf61b9b193aa83d9a6b32857
Jaeyoung <jaeyoungs.park@gmail.com>
2022-11-19 00:00:16 +0900

    [feat] scripts: add service regsteration scripts

---
+++ register_openvpn_service.bat
@@ -0,0 +1,16 @@
+@ECHO off
+
+@REM Elevate your enviroment
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) ^
+              && fsutil dirty query %systemdrive% 1>nul 2>nul ^
+              || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+@REM Register service
+schtasks /CREATE /TN "youngs\OpenVPN Service" ^
+    /TR "'c:\Program Files\OpenVPN\bin\openvpn-gui.exe' --connect incon_agent_proxy_00.ovpn" ^
+    /SC onlogon ^
+    /RL HIGHEST
+
+pause
+exit
